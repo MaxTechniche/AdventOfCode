d6feddf3b4a6098d20356060d92e506b356eeb4d
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2022-11-30 12:39:59 -0600

    Add total stars crop

---
+++ README.md
@@ -0,0 +1,55 @@
+# Advent of Code
+
+This repository contains my solutions to the [Advent of Code](https://adventofcode.com/) puzzles.
+
+## Progress
+
+### Stars
+
+#### 2015
+
+![assets/2015-total.png](assets/2015-total.png)
+
+![assets/2015-stars.png](assets/2015-stars.png)
+
+#### 2016
+
+![assets/2016-total.png](assets/2016-total.png)
+
+![assets/2016-stars.png](assets/2016-stars.png)
+
+#### 2017
+
+![assets/2017-total.png](assets/2017-total.png)
+
+![assets/2017-stars.png](assets/2017-stars.png)
+
+#### 2018
+
+![assets/2018-total.png](assets/2018-total.png)
+
+![assets/2018-stars.png](assets/2018-stars.png)
+
+#### 2019
+
+![assets/2019-total.png](assets/2019-total.png)
+
+![assets/2019-stars.png](assets/2019-stars.png)
+
+#### 2020
+
+![assets/2020-total.png](assets/2020-total.png)
+
+![assets/2020-stars.png](assets/2020-stars.png)
+
+#### 2021
+
+![assets/2021-total.png](assets/2021-total.png)
+
+![assets/2021-stars.png](assets/2021-stars.png)
+
+#### 2022
+
+![assets/2022-total.png](assets/2022-total.png)
+
+![assets/2022-stars.png](assets/2022-stars.png)
--- update-readme.py
+++ update-readme.py
@@ -12,10 +12,19 @@ load_dotenv()
 CURRENT_YEAR = int(time.strftime("%Y"))
 if int(time.strftime("%m")) == 12:
     CURRENT_YEAR += 1
+elif int(time.strftime("%m")) == 11 and int(time.strftime("%d")) > 26:
+    CURRENT_YEAR += 1
 
 SESSION_ID = os.environ.get("SESSION_ID")
 BASE_WEB_URL = "https://adventofcode.com"
-# BASE_WEB_URL = os.environ.get('BASE_WEB_URL')
+WHITE_SQUARE = "assets/white-square.png"
+
+USERNAME = os.environ.get("USERNAME", None)
+USERNAME = "MaxTechniche"
+if isinstance(USERNAME, str) and USERNAME != "":
+    CHARACTERS = len(USERNAME)
+else:
+    CHARACTERS = 0
 
 if BASE_WEB_URL and not BASE_WEB_URL.endswith("/"):
     BASE_WEB_URL += "/"
@@ -25,18 +34,20 @@ options.add_argument("--headless")
 
 service = chrome.service.Service("localhost:4444")
 
-# service = chrome.service.Service(ChromeDriverManager().install())
 # Not needed ?
+# service = chrome.service.Service(ChromeDriverManager().install())
+
 driver = webdriver.Chrome(options=options, service=service)
-WINDOW_SIZE = os.environ.get("WINDOW_SIZE", "1000,1000")
+WINDOW_SIZE = os.environ.get("WINDOW_SIZE", "1200,1000")
 driver.set_window_size(*WINDOW_SIZE.split(","))
 
 driver.get(BASE_WEB_URL)
 driver.add_cookie({"name": "session", "value": SESSION_ID})
 
-for YEAR in range(2015, CURRENT_YEAR):
-    print(f"YEAR: {YEAR}")
+for YEAR in range(2015, CURRENT_YEAR + 1):
+    print(f"YEAR: {YEAR}", file=sys.stderr)
     YEAR = str(YEAR)
+    TOTAL_STARS_FILE = f"assets/{YEAR}-total.png"
     FILENAME = f"assets/{YEAR}-stars.png"
 
     driver.get(BASE_WEB_URL + YEAR)
@@ -45,9 +56,33 @@ for YEAR in range(2015, CURRENT_YEAR):
     with Image.open(FILENAME) as img:
 
         if sys.platform == "darwin":
+            if CHARACTERS > 0:
+                Image.Image.crop(
+                    img,
+                    (
+                        828 * 2 + 12 * (CHARACTERS + 1),
+                        25,
+                        828 * 2 + 12 * (CHARACTERS + 7),
+                        55,
+                    ),
+                ).save(TOTAL_STARS_FILE)
+            else:
+                Image.open(WHITE_SQUARE).save(TOTAL_STARS_FILE)
             Image.Image.crop(img, (0, 150, 1300, 1700)).save(FILENAME)
         else:
+            if CHARACTERS > 0:
+                Image.Image.crop(
+                    img,
+                    (
+                        755 + 12 * (CHARACTERS + 1),
+                        0,
+                        755 + 12 * (CHARACTERS + 5),
+                        30,
+                    ),
+                ).save(TOTAL_STARS_FILE)
+            else:
+                Image.open(WHITE_SQUARE).save(TOTAL_STARS_FILE)
             Image.Image.crop(img, (0, 85, 640, 785)).save(FILENAME)
-    print(f"Saved {FILENAME}")
+    print(f"Saved {FILENAME}", file=sys.stderr)
 
 driver.quit()

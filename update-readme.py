3e745bd7a36ff61049242972c78a9f0314591f93
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2022-11-30 02:10:17 -0600

    woot woot

--- update-readme.py
+++ update-readme.py
@@ -1,28 +1,32 @@
-from selenium import webdriver
-from selenium.webdriver import firefox, chrome
-import time
 import os
-from PIL import Image
-from dotenv import load_dotenv
 import sys
+import time
 
+from dotenv import load_dotenv
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver import chrome
+from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
-SESSION_ID = os.environ.get('SESSION_ID')
-BASE_WEB_URL = os.environ.get('BASE_WEB_URL')
+CURRENT_YEAR = int(time.strftime("%Y"))
+if int(time.strftime("%m")) == 12:
+    CURRENT_YEAR += 1
 
+SESSION_ID = os.environ.get("SESSION_ID")
+BASE_WEB_URL = "https://adventofcode.com"
+# BASE_WEB_URL = os.environ.get('BASE_WEB_URL')
 
-if not BASE_WEB_URL.endswith('/'):
-    BASE_WEB_URL += '/'
+if BASE_WEB_URL and not BASE_WEB_URL.endswith("/"):
+    BASE_WEB_URL += "/"
 
 options = chrome.options.Options()
-options.add_argument('--headless')
+options.add_argument("--headless")
 
-# service = chrome.service.Service('/mnt/c/Users/maxte/Downloads/chromedriver.exe
-# service = chrome.service.Service('localhost:9515')
+service = chrome.service.Service("localhost:4444")
 
-service = chrome.service.Service('/mnt/c/Users/maxte/Downloads/chromedriver')
-driver = webdriver.Chrome(service=service, options=options)
+# service = chrome.service.Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(options=options, service=service)
 
 if WINDOW_SIZE := os.environ.get("WINDOW_SIZE"):
     driver.set_window_size(*WINDOW_SIZE.split(","))
@@ -32,13 +36,16 @@ if WINDOW_POSITION := os.environ.get("WINDOW_POSITION"):
 driver.get(BASE_WEB_URL)
 driver.add_cookie({"name": "session", "value": SESSION_ID})
 
-YEAR = '2015'
+for YEAR in range(2015, CURRENT_YEAR):
+    YEAR = str(YEAR)
+    FILENAME = f"assets/{YEAR}-stars.png"
 
-driver.get(BASE_WEB_URL + YEAR)
-driver.save_screenshot(filename=f'{WINDOW_SIZE}.png')
+    driver.get(BASE_WEB_URL + YEAR)
+    driver.save_screenshot(filename=FILENAME)
+
+    with Image.open(FILENAME) as img:
+
+        Image.Image.crop(img, (0, 150, 1300, 1700)).save(FILENAME)
+        # Image.Image.crop(img, (0, 85, 640, 785)).save(f'{WINDOW_SIZE}.png')
 
 driver.quit()
-
-with Image.open(f'{WINDOW_SIZE}.png') as img:
-
-    Image.Image.crop(img, (0, 85, 640, 785)).save(f'{WINDOW_SIZE}.png')

52b6e61ec023634fc03d685c1bdcd937cc84a403
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2022-12-02 01:11:51 -0600

    Day 2 and update solution copy

--- 2022/Day_02/solution.py
+++ 2022/Day_02/solution.py
@@ -1,4 +1,51 @@
+import pytest
+
 from time import perf_counter
+
+points_1 = {
+    "W": 6,
+    "D": 3,
+    "L": 0,
+    "X": 1,  # Rock
+    "Y": 2,  # Paper
+    "Z": 3,  # Scissors
+}
+
+
+poss_1 = {
+    "A X": "D",
+    "A Y": "W",
+    "A Z": "L",
+    "B X": "L",
+    "B Y": "D",
+    "B Z": "W",
+    "C X": "W",
+    "C Y": "L",
+    "C Z": "D",
+}
+
+points_2 = {
+    "W": 6,
+    "D": 3,
+    "L": 0,
+    "X": 1,  # Rock
+    "Y": 2,  # Paper
+    "Z": 3,  # Scissors
+}
+
+poss_2 = {
+    "A X": ("L", "Z"),
+    "A Y": ("D", "X"),
+    "A Z": ("W", "Y"),
+    "B X": ("L", "X"),
+    "B Y": ("D", "Y"),
+    "B Z": ("W", "Z"),
+    "C X": ("L", "Y"),
+    "C Y": ("D", "Z"),
+    "C Z": ("W", "X"),
+}
+
+
 
 
 def format_time(func):
@@ -11,10 +58,49 @@ def format_time(func):
     return wrapper()
 
 
+def compute1(data):
+    total = 0
+    for line in data.splitlines():
+        opponent, us = line.split()
+        total += points_1[us]
+        total += points_1[poss_1[line.strip()]]
+    return total
+
+
+def compute2(data):
+    total = 0
+    for line in data.splitlines():
+        opponent, us = line.split()
+        total += points_2[poss_2[line][0]]
+        total += points_2[poss_2[line][1]]
+    return total
+
+
 def main():
     with open("2022/Day_02/input.txt") as f:
-        lines = f.read().splitlines()
+        data = f.read()
+    answer = compute1(data)
+    print("Part 1:", answer)
+    answer = compute2(data)
+    print("Part 2:", answer)
 
+
+@pytest.mark.parametrize("input_, expected", [
+    ("""A Y
+B X
+C Z""", 15),
+    ])
+def test1(input_, expected):
+    assert compute1(input_) == expected
+
+
+@pytest.mark.parametrize("input_, expected", [
+    ("""A Y
+B X
+C Z""", 12),
+    ])
+def test2(input_, expected):
+    assert compute2(input_) == expected
 
 if __name__ == "__main__":
     format_time(main)

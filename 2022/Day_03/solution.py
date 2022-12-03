78cbcc565ac8074edeace5e792424727e831b0f2
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2022-12-03 00:55:09 -0600

    day 3

--- 2022/Day_03/solution.py
+++ 2022/Day_03/solution.py
@@ -2,6 +2,10 @@ import pytest
 
 from time import perf_counter
 
+from string import ascii_letters
+
+priorities = {v: i for i, v in enumerate(ascii_letters, 1)}
+
 
 def format_time(func):
     def wrapper(*args, **kwargs):
@@ -9,35 +13,82 @@ def format_time(func):
         func(*args, **kwargs)
         t2 = perf_counter()
         time_delta = t2 - t1
-        print("Time: {:.3f} seconds".format(time_delta))
+        if time_delta * 1000 < 1:
+            print(f"{time_delta * 1000000:.3f} μs")
+        elif time_delta < 1:
+            print(f"{time_delta * 1000:.3f} ms")
+        else:
+            print(f"{time_delta:.3f} s")
+
     return wrapper()
 
 
 def compute1(data):
-    pass
+    rucksacks = data.split("\n")
+    total_priority = 0
+    for rucksack in rucksacks:
+        ruck_len = len(rucksack)
+        left, right = rucksack[: ruck_len // 2], rucksack[ruck_len // 2:]
+        for letter in left:
+            if letter in right:
+                total_priority += priorities[letter]
+                break
+    return total_priority
 
 
 def compute2(data):
-    pass
+    rucksacks = data.split("\n")
+    total_priority = 0
+    for r in range(0, len(rucksacks), 3):
+        rucksack_group = rucksacks[r : r + 3]
+        for letter in rucksack_group[0]:
+            if letter in rucksack_group[1] and letter in rucksack_group[2]:
+                total_priority += priorities[letter]
+                break
+    return total_priority
 
 
 def main():
     with open("2022/Day_03/input.txt") as f:
         data = f.read()
     answer = compute1(data)
-    print(answer)
+    print("Part 1:", answer)
+    answer = compute2(data)
+    print("Part 2:", answer)
 
 
-@pytest.mark.parametrize("input_, expected", [
-    ("", 0),
-    ])
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        (
+            """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            157,
+        ),
+    ],
+)
 def test1(input_, expected):
     assert compute1(input_) == expected
 
 
-@pytest.mark.parametrize("input_, expected", [
-    ("", 0),
-    ])
+@pytest.mark.parametrize(
+    "input_, expected",
+    [
+        (
+            """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+            70,
+        ),
+    ],
+)
 def test2(input_, expected):
     assert compute2(input_) == expected
 

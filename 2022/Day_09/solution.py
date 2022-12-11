9b948ff3bd67cc0d0bb17280405db8a5de54fb9d
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2022-12-10 19:17:02 -0600

    Day 9

--- 2022/Day_09/solution.py
+++ 2022/Day_09/solution.py
@@ -9,16 +9,97 @@ def format_time(func):
         func(*args, **kwargs)
         t2 = perf_counter()
         time_delta = t2 - t1
-        print("Time: {:.3f} seconds".format(time_delta))
+        if time_delta * 1000 < 1:
+            print(str(time_delta * 1000000)[:6], "μs")
+        elif time_delta < 1:
+            print(str(time_delta * 1000)[:6], "ms")
+        else:
+            print(str(time_delta)[:6], "s")
     return wrapper()
 
 
-def compute1(data):
-    pass
+directions = {
+    "U": (0, -1),
+    "D": (0, 1),
+    "R": (1, 0),
+    "L": (-1, 0),
+}
 
 
-def compute2(data):
-    pass
+# def visualize(head, tails, grid_size)
+
+
+def within_one(hx, hy, tx, ty):
+    if abs(hx - tx) <= 1 and abs(hy - ty) <= 1:
+        return True
+    return False
+
+
+def compute1(data, num_tails=1):
+    tail = [0, 0]
+    tail_visited = set()
+    tail_visited.add(tuple(tail))
+    head = [0, 0]
+
+    data = data.splitlines()
+    for line in data:
+        direction, distance = line.strip().split()
+        direction = directions[direction]
+        for x in range(int(distance)):
+            head[0] += direction[0]
+            head[1] += direction[1]
+            if within_one(*head, *tail):
+                continue
+            tail[0] += direction[0]
+            tail[1] += direction[1]
+            if direction[0] != 0:
+                tail[1] = head[1]
+            elif direction[1] != 0:
+                tail[0] = head[0]
+            tail_visited.add(tuple(tail))
+
+    return len(tail_visited)
+
+
+def compute2(data, num_tails=9):
+    head = [0, 0]
+    tails = [list(head) for _ in range(num_tails)]
+    tail_visited = set()
+    tail_visited.add(tuple(head))
+
+    data = data.splitlines()
+    for line in data:
+        direction, distance = line.strip().split()
+        direction = directions[direction]
+        for x in range(int(distance)):
+            head[0] += direction[0]
+            head[1] += direction[1]
+            for tail in range(num_tails):
+                if tail == 0:
+                    if within_one(*head, *tails[tail]):
+                        continue
+                    tails[tail][0] += direction[0]
+                    tails[tail][1] += direction[1]
+                    if direction[0] != 0:
+                        tails[tail][1] = head[1]
+                    elif direction[1] != 0:
+                        tails[tail][0] = head[0]
+                else:
+                    if within_one(*tails[tail-1], *tails[tail]):
+                        continue
+
+                    if tails[tail-1][0] < tails[tail][0]:
+                        tails[tail][0] -= 1
+                    elif tails[tail-1][0] > tails[tail][0]:
+                        tails[tail][0] += 1
+
+                    if tails[tail-1][1] < tails[tail][1]:
+                        tails[tail][1] -= 1
+                    elif tails[tail-1][1] > tails[tail][1]:
+                        tails[tail][1] += 1
+            tail_visited.add(tuple(tails[-1]))
+
+    return len(tail_visited)
 
 
 def main():
@@ -26,17 +107,33 @@ def main():
         data = f.read()
     answer = compute1(data)
     print(answer)
+    answer = compute2(data)
+    print(answer)
 
 
 @pytest.mark.parametrize("input_, expected", [
-    ("", 0),
+    ("""R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2""", 13),
     ])
 def test1(input_, expected):
     assert compute1(input_) == expected
 
 
 @pytest.mark.parametrize("input_, expected", [
-    ("", 0),
+    ("""R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20""", 36),
     ])
 def test2(input_, expected):
     assert compute2(input_) == expected

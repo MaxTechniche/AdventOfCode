c6e7c2ae1bd4ebb8e0029287af5055f57162b8b2
MaxTechniche <22220702+MaxTechniche@users.noreply.github.com>
2024-01-09 14:16:31 -0700

    2023 Blank

---
+++ 2023/Day_13/solution.py
@@ -0,0 +1,52 @@
+import pytest
+
+from time import perf_counter
+
+
+def format_time(func):
+    def wrapper(*args, **kwargs):
+        t1 = perf_counter()
+        func(*args, **kwargs)
+        t2 = perf_counter()
+        time_delta = t2 - t1
+        if time_delta * 1000 < 1:
+            print(str(time_delta * 1000000)[:6], "μs")
+        elif time_delta < 1:
+            print(str(time_delta * 1000)[:6], "ms")
+        else:
+            print(str(time_delta)[:6], "s")
+    return wrapper()
+
+
+def compute1(data):
+    pass
+
+
+def compute2(data):
+    pass
+
+
+def main():
+    with open("2023/Day_13/input.txt") as f:
+        data = f.read()
+    answer = compute1(data)
+    print(answer)
+
+
+@pytest.mark.parametrize("input_, expected", [
+    ("", 0),
+    ])
+def test1(input_, expected):
+    assert compute1(input_) == expected
+
+
+@pytest.mark.parametrize("input_, expected", [
+    ("", 0),
+    ])
+@pytest.mark.skip
+def test2(input_, expected):
+    assert compute2(input_) == expected
+
+
+if __name__ == "__main__":
+    format_time(main)
